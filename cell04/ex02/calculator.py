dd482d7259931f07ccfd02d06351952bde3304a4
Fern220750 <j.jutamanee2207@gmail.com>
2025-06-17 06:23:09 +0000

    calculator

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,21 @@
+def main():
+    try:
+       
+        num1 = float(input("Give me the first number: "))
+        num2 = float(input("Give me the second number: "))
+        
+        print("Thank you!")
+        
+       
+        print(f"{num1} + {num2} = {num1 + num2}")
+        print(f"{num1} - {num2} = {num1 - num2}")
+        print(f"{num1} / {num2} = {num1 / num2}")
+        print(f"{num1} * {num2} = {num1 * num2}")
+        
+    except ValueError:
+        print("Please enter valid numbers.")
+    except ZeroDivisionError:
+        print("Division by zero is not allowed.")
+
+if __name__ == "__main__":
+    main()
