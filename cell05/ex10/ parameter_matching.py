3df52049772998662ff8ff151118353136f9a19d
Fern220750 <j.jutamanee2207@gmail.com>
2025-06-18 02:09:23 +0000

    count

--- cell05/ex10/ parameter_matching.py
+++ cell05/ex10/ parameter_matching.py
@@ -1,17 +1,13 @@
-#!/usr/bin/env python3
 import sys
 
-# Check if there is exactly one argument passed to the script
 if len(sys.argv) != 2:
     print("none")
 else:
-    # The argument passed is stored in sys.argv[1]
+
     param = sys.argv[1]
 
-    # Prompt the user to enter a word
     word = input("What was the parameter? ")
 
-    # Compare the entered word with the parameter
     if word == param:
         print("Good job!")
     else:
